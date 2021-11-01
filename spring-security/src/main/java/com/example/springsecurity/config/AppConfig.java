7b56f21d8acb5b26242cf5250a29dcd936e69d88
Abhishek Anand <46523310+abhik26@users.noreply.github.com>
2021-11-01 19:24:53 +0530

    Spring Security | user authentication using database

--- spring-security/src/main/java/com/example/springsecurity/config/AppConfig.java
+++ spring-security/src/main/java/com/example/springsecurity/config/AppConfig.java
@@ -1,16 +1,32 @@
 package com.example.springsecurity.config;
 
+import java.beans.PropertyVetoException;
+import java.util.logging.Logger;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages="com.example.springsecurity")
+@PropertySource("classpath:persistence.properties")
 public class AppConfig {
+	
+	@Autowired
+	private Environment env;
+	
+	private Logger log = Logger.getLogger(getClass().getName());
 
 	@Bean
 	public ViewResolver viewResolver() {
@@ -18,5 +34,36 @@ public class AppConfig {
 		viewResolver.setPrefix("/WEB-INF/view/");
 		viewResolver.setSuffix(".jsp");
 		return viewResolver;
+	}
+	
+	@Bean
+	public DataSource dataSource() {
+		ComboPooledDataSource dataSource = 
+				new ComboPooledDataSource();
+		
+		try {
+			dataSource.setDriverClass(env.getProperty("jdbc.driver"));
+		} catch (PropertyVetoException e) {
+			throw new RuntimeException(e);
+		}
+		
+		log.info(">> jdbc.url=" + env.getProperty("jdbc.url"));
+		log.info(">> jdbc.user=" + env.getProperty("jdbc.user"));
+		
+		dataSource.setJdbcUrl(env.getProperty("jdbc.url"));
+		dataSource.setUser(env.getProperty("jdbc.user"));
+		dataSource.setPassword(env.getProperty("jdbc.password"));
+		
+		dataSource.setInitialPoolSize(getIntProperty("connection.pool.initialPoolSize"));
+		dataSource.setMinPoolSize(getIntProperty("connection.pool.minPoolSize"));
+		dataSource.setMaxPoolSize(getIntProperty("connection.pool.maxPoolSize"));
+		dataSource.setMaxIdleTime(getIntProperty("connection.pool.maxIdleTime"));
+		
+		return dataSource;
+	}
+	
+	private int getIntProperty(String propName) {
+		String propValue = env.getProperty(propName);
+		return Integer.parseInt(propValue);
 	}
 }
--- spring-security/src/main/java/com/example/springsecurity/config/SecurityConfig.java
+++ spring-security/src/main/java/com/example/springsecurity/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.example.springsecurity.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,16 +14,21 @@ import org.springframework.security.core.userdetails.User.UserBuilder;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private DataSource dataSource;
 
-	@SuppressWarnings("deprecation")
+//	@SuppressWarnings("deprecation")
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		UserBuilder users = User.withDefaultPasswordEncoder();
-		
-		auth.inMemoryAuthentication()
-				.withUser(users.username("john").password("test123").roles("EMPLOYEE"))
-				.withUser(users.username("mary").password("test123").roles("EMPLOYEE", "MANAGER"))
-				.withUser(users.username("abhishek").password("test123").roles("EMPLOYEE", "ADMIN"));
+//		UserBuilder users = User.withDefaultPasswordEncoder();
+//		
+//		auth.inMemoryAuthentication()
+//				.withUser(users.username("john").password("test123").roles("EMPLOYEE"))
+//				.withUser(users.username("mary").password("test123").roles("EMPLOYEE", "MANAGER"))
+//				.withUser(users.username("abhishek").password("test123").roles("EMPLOYEE", "ADMIN"));
+	
+		auth.jdbcAuthentication().dataSource(dataSource);
 	}
 
 	@Override
